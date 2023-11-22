e5aaf7139d859977f497268ec534b4adf868d0b8
Aku Sarma <akusarma1702@gmail.com>
2023-11-22 20:33:43 +0530

    deleted varcel requirements

--- technogyan/settings.py
+++ technogyan/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^clrj8d-(6f5t30!5yl4u#3d6g@q9zugvogh(-!1)0$h&f2@d0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.varcel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', "static") # requires collectstatic command
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # requires collectstatic command
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
